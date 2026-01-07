d93857c7ce2505fbea87a4bfc6679afba76879c9
Manorama <m4manorama008@gmail.com>
2026-01-07 10:48:56 +0530

    07-01-2026-1

---
+++ src/main/java/question/example/test/LTIMCBS.java
@@ -0,0 +1,25 @@
+package question.example.test;
+
+public class LTIMCBS {
+
+    public static void main(String[] args) {
+    String input="Manorama Singh";
+        String result=alternateCharacter(input);
+        System.out.println(result);
+    }
+    public static String alternateCharacter(String input){
+        String [] word=input.split(" ");
+        StringBuilder sb=new StringBuilder();
+        int max=Math.max(word[0].length(),word[1].length());
+        for(int i=0;i<max;i++){
+         if(i<word[0].length()){
+            sb.append(Character.toLowerCase(word[0].charAt(i)));
+         }
+         if(i<word[1].length()){
+             sb.append(Character.toLowerCase(word[1].charAt(i)));
+         }
+        }
+
+        return sb.toString();
+    }
+}
